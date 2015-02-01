447451140a8352db4375d0eac22dbce5b00cb84e
Simon Uyttendaele <simon@busit.com>
2015-02-01 23:49:01 +0100

    settings

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 function createShortcutMenu()
 {
+	var quick = document.createElement("DIV");
+	quick.id = "quick";
+	document.body.appendChild(quick);
+	
 	$("h2").each(function(i, e)
 	{
 		var a = document.createElement('A');
 		a.href = '#' + e.parentElement.name;
 		a.innerHTML = e.innerHTML;
-		$('#quick').append(a);
+		quick.appendChild(a);
 	});
 }
