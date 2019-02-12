ecb71c4f2eef1c3938e945999d32850cf68e6cb5
Simon Uyttendaele <simon@busit.com>
2019-02-12 15:15:04 +0100

    js api doc

--- js/jsapidoc.js
+++ js/jsapidoc.js
@@ -31,22 +31,25 @@ window.jsapidoc = bi.instance(
 			for( var i = 0; i < this.content.length; i++ )
 			{
 				this.content[i].items.sort(function(a, b) { return (a.name > b.name ? 1 : -1); });
-				var li = ol.appendChild(bi.node('li', this.content[i].name.escape(), {click: function() { this.classList.toggle('open'); }}));
+				var li = ol.appendChild(bi.node('li', bi.node('span', this.content[i].name.escape(), {click: function() { this.classList.toggle('open'); }})));
 				var ol2 = li.appendChild(bi.node('ol'));
 				
 				for( var j = 0; j < this.content[i].items.length; j++ )
 				{
-					ol2.appendChild(bi.node('li', this.content[i].items[j].name,
+					ol2.appendChild(bi.node('li', bi.node('span', this.content[i].items[j].name.escape(),
 					{
-						click: function() { self.showClass(this.dataset.package, this.dataset.class); }
+						click: function() { self.showClass(this.dataset.package, this.dataset.class); },
 						dataPackage: i,
 						dataClass: j
-					}));
+					})));
 				}
 			}
 		},
-		showClass: function(name)
+		showClass: function(p, c)
 		{
+			var main = bi.$('jsapidoc_main');
+			main.clear();
+			main.appendChild(bi.node('h1', this.content[p].items[c].name.escape()));
 		}
 	}
 });
