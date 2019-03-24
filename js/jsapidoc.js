93d3d007620c1c94c72444dfaf3fbb3dfbebd5d0
Simon Uyttendaele <simon@busit.com>
2019-03-24 13:52:16 +0100

    jsapi

--- js/jsapidoc.js
+++ js/jsapidoc.js
@@ -94,7 +94,7 @@ window.jsapidoc = bi.instance(
 						])),
 						bi.node('tbody', bi.node('tr',
 						[
-							bi.node('td', item.ctor.signature.replace(/ ([a-z0-9_])+\(/i, " <strong>$1</strong>(")),
+							bi.node('td', item.ctor.signature.replace(/(^| )([a-z0-9_$]+)\(/i, "$1<strong>$2</strong>(")),
 							bi.node('td', item.ctor.description)
 						]))
 					])
